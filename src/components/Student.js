13ae60c8b420a66257993d167b4308e78945f6c6
Devin Helmgren <devincollege@gmail.com>
2019-11-25 12:07:50 -0800

    oops, birthday

--- src/components/Student.js
+++ src/components/Student.js
@@ -7,21 +7,35 @@ class Student extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      fullName: props.fullName,
+      pronouns: props.pronouns,
+      birthday: props.birthday
+    }
+  }
+
+  oopsBirthday(){
+
+    this.setState({birthday: 'Oct 22nd'})
   }
 
   render() {
 
     console.log('the value of props is', this.props);
 
+    //this.state.birthday = 'Oct 22nd'
+
     // I want to pass specific data from A to B. The format of this data is a name/value pair.
     // To SEND the data from A to B, when I use the <>, I will pass every name/value pair like they are attributes
     return (
       <section>
-        <h2 className="student-name">{this.props.fullName} -- "{this.props.nickName}"</h2>
+        <h2 className="student-name">{this.state.fullName} -- "{this.props.nickName}"</h2>
         <ul className="student-details">
-          <li>Pronouns: {this.props.pronouns}</li>
-          <li>Birthday: {this.props.birthday}</li>
+          <li>Pronouns: {this.state.pronouns}</li>
+          <li>Birthday: {this.state.birthday}</li>
         </ul>
+        {this.oopsBirthday()}
       </section>
     );
   }
