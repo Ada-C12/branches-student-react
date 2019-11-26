0efebdea6a907c1fd48275dd642bef09dedbebf7
dee del rosario <dee@adadevelopersacademy.org>
2019-11-26 09:29:30 -0800

    converts studentcollection comp to classical

--- src/components/StudentCollection.js
+++ src/components/StudentCollection.js
@@ -1,47 +1,52 @@
 import React from 'react';
 import Student from './Student';
 
-const StudentCollection = () => {
+class StudentCollection extends React.Component {
 
-  const students = [
-    {
-      name: "Ada",
-      nickName: "The Lovelacer",
-      pronouns: 'They/Them'
-    },
-    {
-      name: "Katherine",
-      nickName: "\"out of this world\"",
-      pronouns: 'She/Her'
-    },
-  ];
+  constructor() {
+    super();
 
-  const studentComponents = students.map((student, i) => {
-    const demographics = {
-      age: 21,
-      grade: 'Junior',
-      house: 'Slytheryn',
+    this.state = {
+      students: [
+        {
+          name: "Ada",
+          nickName: "The Lovelacer",
+          pronouns: 'They/Them',
+          birthday: 'October?????',
+        },
+        {
+          name: "Katherine",
+          nickName: "\"out of this world\"",
+          pronouns: 'She/Her',
+          birthday: 'March.'
+        },
+      ],
     }
+  }
+
+  render() {
+    const studentComponents = this.state.students.map((student, i) => {
+      return (
+        <li key={i}>
+          <Student
+            fullName={student.name}
+            nickName={student.nickName}
+            pronouns={student.pronouns}
+          />
+        </li>
+      )
+    });
+  
     return (
-      <li key={i}>
-        <Student
-          fullName={student.name}
-          nickName={student.nickName}
-          pronouns={student.pronouns}
-        />
-      </li>
-    )
-  });
-
-  return (
-    <section>
-      <h2>Student Collection</h2>
-
-      <ul>
-        {studentComponents}
-      </ul>
-    </section>
-  );
+      <section>
+        <h2>Student Collection</h2>
+  
+        <ul>
+          {studentComponents}
+        </ul>
+      </section>
+    );
+  }
 }
 
 export default StudentCollection;
