2187bc48d1811b3489d22ed48c0153abd8b6d66e
CheezItMan <chris@adadevelopersacademy.org>
2019-11-25 18:51:27 -0800

    update App Component to pass in student data to collection

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import './App.css';
 import StudentCollection from './components/StudentCollection';
 import Link from './components/Link';
 
+const students = [
+  {
+    id: 1,
+    name: "Ada",
+    nickName: "The Lovelacer",
+    pronouns: 'They/Them',
+    displayPronouns: false,
+  },
+  {
+    id: 2,
+    name: "Katherine",
+    nickName: "\"out of this world\"",
+    pronouns: 'She/Her',
+    displayPronouns: false,
+  },
+]
+
 function App () {
   return (
     <div className="App">
       <h1>Welcome to the Student React App!</h1>
-      <StudentCollection />
+      <StudentCollection students={students} />
     </div>
   );
 }
