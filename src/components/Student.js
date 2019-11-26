eb2a9e1c7ab8395439e5715d167c6a0d5f2f5bc7
CheezItMan <chris@adadevelopersacademy.org>
2019-11-25 18:53:40 -0800

    refactor Student Component into functional component using callback function as a prop

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,56 +3,36 @@ import PropTypes from 'prop-types';
 
 import './Student.css'
 
-class Student extends React.Component {
+const togglePronounsDisplay = (props) => {
+  console.log('Toggling display of pronouns', props);
+  props.toggleDisplayPronouns(props.id);
+}
 
-  constructor(props) {
-    super(props);
+const onBirthdayChange = () => {
+  console.log('onBirthdayChange');
+}
 
-    this.state = {
-      fullName: props.fullName,
-      pronouns: props.pronouns,
-      birthday: props.birthday
-    }
+const Student = (props) => {
+
+  const { birthday, fullName, nickName } = props;
+
+  let pronouns = null;
+  if (props.displayPronouns) {
+    pronouns = <li>Pronouns: {props.pronouns}</li>;
   }
 
-  onBirthdayChange = (event) => {
-    this.setState({ 
-      birthday: event.target.value
-    });
-  }
-
-  togglePronounsDisplay = () => {
-    this.setState({
-      displayPronouns: !this.state.displayPronouns,
-
-    })
-  }
-
-  render = () => {
-
-    console.log('the value of props is', this.props);
-
-    //this.state.birthday = 'Oct 22nd'
-
-    // I want to pass specific data from A to B. The format of this data is a name/value pair.
-    // To SEND the data from A to B, when I use the <>, I will pass every name/value pair like they are attributes
-    let pronouns = null;
-    if (this.state.displayPronouns) {
-      pronouns = <li>Pronouns: {this.state.pronouns}</li>;
-    }
-    
-    return (
-      <section>
-        <h2 className="student-name">{this.state.fullName} -- "{this.props.nickName}"</h2>
-        <ul className="student-details">
-          {pronouns}
-          <li>Birthday: {this.state.birthday}</li>
-        </ul>
-        <button onClick={this.togglePronounsDisplay}>Toggle Display of Pronouns</button>
-        <p>Change your birthday: <input onChange={ this.onBirthdayChange }></input></p>
-      </section>
-    );
-  }
+  return (
+    <section>
+      <h2 className="student-name">{fullName} -- "{nickName}"</h2>
+      <ul className="student-details">
+        {pronouns}
+        <li>Birthday: {birthday}</li>
+      </ul>
+      <button onClick={() => togglePronounsDisplay(props)}>Toggle Display of Pronouns</button>
+      {pronouns ? pronouns : ""}
+      <p>Change your birthday: <input onChange={() => onBirthdayChange()}></input></p>
+    </section>
+  );
 }
 
 Student.propTypes = {
@@ -60,6 +40,7 @@ Student.propTypes = {
   nickName: PropTypes.string.isRequired,
   pronouns: PropTypes.string.isRequired,
   birthday: PropTypes.string,
+  displayPronouns: PropTypes.bool,
   demos: PropTypes.shape({
     age: PropTypes.number.isRequired,
     grade: PropTypes.string,
